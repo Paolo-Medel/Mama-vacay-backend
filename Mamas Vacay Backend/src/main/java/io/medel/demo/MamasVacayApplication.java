f9b6fb234fd04614ae7b5e182444fdeb67555c52
paolomedel <paolomedel17@gmail.com>
2025-10-25 14:18:33 -0400

    initial spring boot project made with spring initializr

---
+++ Mamas Vacay Backend/src/main/java/io/medel/demo/MamasVacayApplication.java
@@ -0,0 +1,13 @@
+package io.medel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamasVacayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamasVacayApplication.class, args);
+	}
+
+}
---
+++ Mamas Vacay Backend/src/test/java/io/medel/demo/MamasVacayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.medel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamasVacayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
